a8d8458f8ada6820981996cb81e34284e79dabe3
lothux1987 <14127085+lutherjohn@users.noreply.github.com>
2020-08-14 11:59:54 +0800

    Add files via upload

---
+++ application/views/footer.php
@@ -0,0 +1,17 @@
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+	
+	
+	<!-- Copyright -->
+    <div class="footer-copyright text-center py-3">© 2020 Copyright: Ordering System version 1.0   </div>
+  <!-- Copyright -->
+    <script src='<?php echo base_url() . "bootstrap/js/bootstrap.bundle.js" ?>' type="text/javascript"></script>
+    <script src='<?php echo base_url() . "bootstrap/js/bootstrap.bundle.min.js" ?>' type="text/javascript"></script>
+    <script src='<?php echo base_url() . "bootstrap/js/bootstrap.js" ?>' type="text/javascript"></script>
+    <script src='<?php echo base_url() . "bootstrap/js/bootstrap.min.js" ?>' type="text/javascript"></script>
+
+	
+
+</body>
+</html>
---
+++ application/views/header.php
@@ -0,0 +1,24 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Ordering System</title>
+	<link href='<?php echo base_url() ."bootstrap/css/bootstrap.min.css";?>' rel="stylesheet">
+	<link href='<?php echo base_url() ."bootstrap/css/bootstrap-grid.css";?>' rel="stylesheet">
+	<link href='<?php echo base_url() ."bootstrap/css/bootstrap-grid.min.css";?>' rel="stylesheet">
+	<link href='<?php echo base_url() ."bootstrap/css/bootstrap-reboot.css";?>' rel="stylesheet">
+	<link href='<?php echo base_url() ."bootstrap/css/bootstrap-reboot.min.css";?>' rel="stylesheet">
+    
+
+
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
+
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js" type="text/javascript"></script>
+    <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
+
+</head>
+<body>
